26892a12df4e3d99d0b719e254cb4675982a71fe
Mult0 <taezarex@gmail.com>
2025-07-20 17:24:47 +0800

    Add project files.

---
+++ GetUserInput.cs
@@ -0,0 +1,47 @@
+﻿namespace CodingTracker
+{
+    internal class GetUserInput
+    {
+
+        internal void MainMenu()
+        {
+            bool closeApp = false;
+
+            while (closeApp == false)
+            {
+                Console.WriteLine("MAIN MENU");
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("\t-Type 0 to close the app");
+                Console.WriteLine("\t-Type 1 to view records");
+                Console.WriteLine("\t-Type 2 to add a record");
+                Console.WriteLine("\t-Type 3 to update a record");
+                Console.WriteLine("\t-Type 4 to delete a record");
+
+                String? userCommand = Console.ReadLine();
+
+                while (string.IsNullOrEmpty(userCommand)) 
+                {
+                    Console.WriteLine("Invalid, please enter a number from 0-4");
+                    userCommand = Console.ReadLine();
+                }
+
+                switch(userCommand)
+                {
+                    case "0":
+
+                        closeApp = true;
+                        break;
+                    case "1":
+
+                        break;
+
+                    case "2":
+
+                        break;
+
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CodingTracker
+{
+    internal class Program
+    {
+
+        static string connectionString = @"Data Source=coding-tracker.db";
+
+
+        static void Main(string[] args)
+        {
+            
+
+            DataBaseManager dataBaseManager = new();
+            GetUserInput getUserInput = new();
+            dataBaseManager.CreateTable(connectionString);
+
+        }
+    }
+}
